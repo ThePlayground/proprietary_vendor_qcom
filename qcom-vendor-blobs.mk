7171146c81177eb60205e14b27055d90f74e90c6
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-03 22:19:46 -0400

    Adreno200-AU_LINUX_ANDROID_ICS_CHOCO_CS.04.00.03.06.001

--- qcom-vendor-blobs.mk
+++ qcom-vendor-blobs.mk
@@ -1,5 +1,4 @@
-# Copyright (C) 2012 Qualcomm
-# Copyright (C) 2012 Twisted Playground
+# Copyright (C) 2012 Qualcomm Developer Network
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
